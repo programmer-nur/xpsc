60e2feb92eb22eee14ac175d736788b8bb0382e1
programmer-nur <pesfootball83@gmail.com>
2026-02-16 18:43:10 +0600

    added biscuit count

---
+++ week-1/A_Biscuit_Generator.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int a, b, t;
+    cin >> a >> b >> t;
+
+    int count = t / a;
+
+    
+    int prod = count * b;
+
+    cout << prod << endl;
+
+    return 0;
+}
